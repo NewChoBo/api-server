af8d009d02920b23bc6aefa2cdaf08aceecbac70
NewChoBo <whwjdrb1009@gmail.com>
2024-10-27 10:09:32 +0900

    Initial commit

---
+++ src/main/java/jjk/api/api_server/ApiServerPrototypeApplication.java
@@ -0,0 +1,13 @@
+package jjk.api.api_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiServerPrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiServerPrototypeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jjk/api/api_server/ApiServerPrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package jjk.api.api_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServerPrototypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

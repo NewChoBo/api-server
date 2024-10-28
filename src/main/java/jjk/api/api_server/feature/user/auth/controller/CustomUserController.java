7a4d0f18dc150b3a3dfd925d65847fd0a8aa8fdc
NewChoBo <whwjdrb1009@gmail.com>
2024-10-28 23:38:28 +0900

    refactor(debug): 로거 사용하도록 설정

--- src/main/java/jjk/api/api_server/feature/user/auth/controller/CustomUserController.java
+++ src/main/java/jjk/api/api_server/feature/user/auth/controller/CustomUserController.java
@@ -4,6 +4,8 @@ import jjk.api.api_server.common.util.JwtUtil;
 import jjk.api.api_server.feature.user.auth.dto.LoginDto;
 import jjk.api.api_server.feature.user.auth.model.CustomUserDetails;
 import jjk.api.api_server.feature.user.auth.service.CustomUserDetailsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -16,6 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/auth")
 public class CustomUserController {
+
+  private static final Logger logger = LoggerFactory.getLogger(CustomUserController.class);
 
   private final AuthenticationManager authenticationManager;
   private final CustomUserDetailsService userService;
@@ -35,7 +39,7 @@ public class CustomUserController {
           new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword())
       );
     } catch (AuthenticationException e) {
-      e.printStackTrace();
+      logger.error("Invalid username or password", e);
       return ResponseEntity.status(401).body("Invalid username or password");
     }
 

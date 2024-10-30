a3939ae22f7af3a9f6c43de7fdb620e221c3dc86
NewChoBo <whwjdrb1009@gmail.com>
2024-10-30 22:45:13 +0900

    Revert "refactor(auth): 컨트롤러 이름 변경"
    This reverts commit edd768e205737f77e6a1a022cd2e999da138e6e0.

---
+++ src/main/java/jjk/api/api_server/feature/user/auth/controller/CustomUserController.java
@@ -0,0 +1,52 @@
+package jjk.api.api_server.feature.user.auth.controller;
+
+import jjk.api.api_server.common.util.JwtUtil;
+import jjk.api.api_server.feature.user.auth.dto.LoginDto;
+import jjk.api.api_server.feature.user.auth.model.CustomUserDetails;
+import jjk.api.api_server.feature.user.auth.service.CustomUserDetailsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class CustomUserController {
+
+  private static final Logger log = LoggerFactory.getLogger(CustomUserController.class);
+
+  private final AuthenticationManager authenticationManager;
+  private final CustomUserDetailsService userService;
+  private final JwtUtil jwtUtil;
+
+  public CustomUserController(AuthenticationManager authenticationManager,
+      CustomUserDetailsService userService, JwtUtil jwtUtil) {
+    this.authenticationManager = authenticationManager;
+    this.userService = userService;
+    this.jwtUtil = jwtUtil;
+  }
+
+  @PostMapping("/signin")
+  public ResponseEntity<String> signIn(@RequestBody LoginDto loginDto) {
+    try {
+      authenticationManager.authenticate(
+          new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword())
+      );
+    } catch (AuthenticationException e) {
+      log.error("Invalid username or password", e);
+      return ResponseEntity.status(401).body("Invalid username or password");
+    }
+
+    final CustomUserDetails userDetails = userService.loadUserByUsername(loginDto.getUsername());
+    final String jwt = jwtUtil.generateToken(userDetails);
+
+    return ResponseEntity.ok(jwt);
+  }
+
+}
--- src/main/java/jjk/api/api_server/feature/user/auth/service/CustomUserDetailsService.java
+++ src/main/java/jjk/api/api_server/feature/user/auth/service/CustomUserDetailsService.java
@@ -3,62 +3,23 @@ package jjk.api.api_server.feature.user.auth.service;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
-import jjk.api.api_server.common.util.JwtUtil;
-import jjk.api.api_server.feature.user.auth.dto.LoginDto;
 import jjk.api.api_server.feature.user.auth.model.CustomUserDetails;
 import jjk.api.api_server.feature.user.user.dto.RoleDto;
 import jjk.api.api_server.feature.user.user.dto.UserDto;
 import jjk.api.api_server.feature.user.user.service.UserService;
-import org.modelmapper.ModelMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
-  private static final Logger log = LoggerFactory.getLogger(CustomUserDetailsService.class);
-
   private final UserService userService;
-  private final ModelMapper modelMapper;
-  private final AuthenticationManager authenticationManager;
-  private final JwtUtil jwtUtil;
 
-  public CustomUserDetailsService(UserService userService, ModelMapper modelMapper,
-      AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
+  public CustomUserDetailsService(UserService userService) {
     this.userService = userService;
-    this.modelMapper = modelMapper;
-    this.authenticationManager = authenticationManager;
-    this.jwtUtil = jwtUtil;
-  }
-
-  public ResponseEntity<String> signIn(@RequestBody LoginDto loginDto) {
-    try {
-      authenticationManager.authenticate(
-          new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword()));
-    } catch (AuthenticationException e) {
-      log.error("Invalid username or password", e);
-      return ResponseEntity.status(401).body("Invalid username or password");
-    }
-
-    final CustomUserDetails userDetails = this.loadUserByUsername(loginDto.getUsername());
-    final String jwt = jwtUtil.generateToken(userDetails);
-
-    return ResponseEntity.ok(jwt);
-  }
-
-  public void signUp(LoginDto loginDto) {
-    UserDto userDto = modelMapper.map(loginDto, UserDto.class);
-    userService.createUser(userDto);
   }
 
   @Override
@@ -76,7 +37,8 @@ public class CustomUserDetailsService implements UserDetailsService {
   }
 
   Set<GrantedAuthority> getAuthByRoles(Set<RoleDto> roles) {
-    return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName()))
+    return roles.stream()
+        .map(role -> new SimpleGrantedAuthority(role.getName()))
         .collect(Collectors.toSet());
   }
 }

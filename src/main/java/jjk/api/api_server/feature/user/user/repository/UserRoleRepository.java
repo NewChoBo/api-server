420d237967ee906ecb7467243faf9c0a271ffe53
조 정규 <regularcho1009@metabuild.co.kr>
2024-11-06 18:17:59 +0900

    fix(mapping): 잘못 생성한 구현체 제거

--- src/main/java/jjk/api/api_server/feature/user/user/repository/UserRoleRepository.java
+++ src/main/java/jjk/api/api_server/feature/user/user/repository/UserRoleRepository.java
@@ -1,11 +1,8 @@
 package jjk.api.api_server.feature.user.user.repository;
 
-import java.util.Optional;
-import jjk.api.api_server.feature.user.user.entity.User;
 import jjk.api.api_server.feature.user.user.entity.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
 
-  Optional<User> findByLoginId(String loginId);
 }

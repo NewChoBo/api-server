23f9ede3d5979e983fbda78ba5ef90f2fc515f20
NewChoBo <whwjdrb1009@gmail.com>
2024-11-07 21:47:20 +0900

    refactor(mapping): ManyToMany 복구
    변수명 수정

--- src/main/java/jjk/api/api_server/feature/user/auth/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package jjk.api.api_server.feature.user.auth.repository;
-
-import jjk.api.api_server.feature.user.user.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-
-}
